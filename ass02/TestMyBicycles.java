23687260693ed6c28cf21eb19cea342a34b1a1a6
Aryan Gupta <kuantum.freak@gmail.com>
2020-10-05 16:14:31 -0400

    Add TestMyBicycles test class

---
+++ ass02/TestMyBicycles.java
@@ -0,0 +1,39 @@
+import java.sql.SQLException;
+
+public final class TestMyBicycles {
+    public static void main(String[] args) throws SQLException {
+        String hostip = BikesUtil.getHost();
+        String passwd = BikesUtil.getPasswd();
+
+        TableAttributes ta = new TableAttributes();
+        ta.type = "race_bike";
+        ta.color = "black";
+        ta.material = "carbon";
+        ta.gears = 6;
+        ta.sort = TableAttributes.ATTRIBUTES.HEIGHT;
+        System.out.println("Search for bicycles");
+        (new SearchTable(hostip, passwd)).run(ta);
+
+
+        ta.wheelBase = 42;
+        ta.height = 2;
+        System.out.println("");
+        System.out.println("Insert a bicycle");
+        (new InsertRow(hostip, passwd)).run(ta);
+
+        System.out.println("");
+        System.out.println("Delete all bicycle");
+        (new DeleteRow(hostip, passwd)).run(ta);
+
+        System.out.println("");
+        System.out.println("Re-add one bicycle");
+        (new InsertRow(hostip, passwd)).run(ta);
+
+        ta.wheelBase = -1;
+        ta.height = -1;
+        ta.sort = TableAttributes.ATTRIBUTES.WHEEL_BASE;
+        System.out.println("");
+        System.out.println("Search Bicycles again");
+        (new SearchTable(hostip, passwd)).run(ta);
+    }
+}
