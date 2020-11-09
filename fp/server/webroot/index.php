adf6d8cd1be1177ad65809bae1e25c5202f52bdc
Aryan Gupta <kuantum.freak@gmail.com>
2020-11-08 20:33:59 -0500

    Add show/hide password option on login page

--- fp/server/webroot/index.php
+++ fp/server/webroot/index.php
@@ -6,11 +6,23 @@
 <body>
 <?php
 if (!isset($_SESSION["username"])) {
-    echo '<form action="login.php" method="post">';
-    echo '<input type="text" name="username" placeholder="Username"><br/>';
-    echo '<input type="text" name="password" placeholder="Password"><br/>';
-    echo '<input type="submit">';
-    echo '</form>';
+    echo '
+    <script>
+      function togglePasswordVisibility() {
+        var x = document.getElementById("password");
+        if (x.type === "password") {
+          x.type = "text";
+        } else {
+          x.type = "password";
+        }
+      }
+    </script>
+    <form action="login.php" method="post">
+        <input type="text" name="username" placeholder="Username"><br/>
+        <input type="text" name="password" placeholder="Password" id="password"><br/>
+        <input type="checkbox" onclick="togglePasswordVisibility()" checked>Show Password <br/>
+        <input type="submit">
+    </form>';
 } else {
     $username = $_SESSION['username'];
     echo "Welcome $username";
