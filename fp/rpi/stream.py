0cbcf12463df4368f6d9589236c4118a76a8e3a5
Aryan Gupta <kuantum.freak@gmail.com>
2020-11-23 10:29:44 -0500

    Add webcam stream timing diag

--- fp/rpi/stream.py
+++ fp/rpi/stream.py
@@ -63,27 +63,46 @@ if not ret:
 frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 detector = MotionDetector(frame)
 
+frame_num = 0
+gray_total = 0
+bbox_total = 0
+dete_total = 0
+enco_total = 0
+
 def gen(): 
 	"""Video streaming generator function.""" 
 
 	global vc
+	global frame_num, gray_total, bbox_total, dete_total, enco_total
 
 	while True: 
+		frame_num += 1
 		rval, frame = vc.read()
+		
+		start_time = time.time()
 		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		gray_total += (time.time() - start_time)
 
 		if not rval:
 			print("Can't receive frame (stream end?). Exiting ...")
 			break
 
+		start_time = time.time()
 		bounding_box = detector.get_motion_bounding_box(gray)
+		bbox_total += (time.time() - start_time)
+
+		start_time = time.time()
 		detector.update(gray)
+		dete_total += (time.time() - start_time)
 
 		if bounding_box is not None:
 			(thresh, (minX, minY, maxX, maxY)) = bounding_box
 			cv2.rectangle(frame, (minX, minY), (maxX, maxY), (0, 0, 255), 1)
 
+		start_time = time.time()
 		(flag, encodedImage) = cv2.imencode(".jpg", frame) 
+		enco_total += (time.time() - start_time)
+
 		if not flag:
 			continue
 
@@ -104,3 +123,19 @@ def video_feed():
 
 app.run(host='0.0.0.0', port=8081, debug=False, threaded=True) 
 vc.release()
+
+# These are the timing values
+# gray: 0.00015909435184857318
+# bbox: 0.0017290442954492934
+# dete: 0.0003856199084954104
+# enco: 0.014365608758902125
+# even if we pipeline it by taking the fastest 3 stages 
+# and the 1 fastest stage, then we would only get an 16%
+# improvement. Considering that this is tested on my 
+# i7-4790K non-OC, it might be beneficial to thread it
+# once we get it on a rpi
+print()
+print("gray: %s" % (gray_total / frame_num))
+print("bbox: %s" % (bbox_total / frame_num))
+print("dete: %s" % (dete_total / frame_num))
+print("enco: %s" % (enco_total / frame_num))
