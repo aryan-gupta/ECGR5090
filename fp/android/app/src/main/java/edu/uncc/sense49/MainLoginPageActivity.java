b57201630ab8da07f7a6249291fcb9bbf5bebd6b
Aryan Gupta <kuantum.freak@gmail.com>
2020-12-09 14:42:21 -0500

    Attempt for login

--- fp/android/app/src/main/java/edu/uncc/sense49/MainLoginPageActivity.java
+++ fp/android/app/src/main/java/edu/uncc/sense49/MainLoginPageActivity.java
@@ -4,8 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class MainLoginPageActivity extends AppCompatActivity {
 
@@ -16,12 +29,45 @@ public class MainLoginPageActivity extends AppCompatActivity {
     }
 
     /** Called when the user taps the Send button */
-    public void login(View view) {
+    public void login(View view) throws MalformedURLException, UnsupportedEncodingException, IOException {
         EditText usernameText = (EditText) findViewById(R.id.mainUsernameTextBox);
         EditText passwordText = (EditText) findViewById(R.id.mainPasswordTextBox);
 
         String username = usernameText.getText().toString();
         String password = passwordText.getText().toString();
+
+//        URL url = new URL("http://172.18.0.4:80/login.php");
+//        Map<String,Object> params = new LinkedHashMap<>();
+//        params.put("username", username);
+//        params.put("password", password);
+//
+//        StringBuilder postData = new StringBuilder();
+//        for (Map.Entry<String,Object> param : params.entrySet()) {
+//            if (postData.length() != 0) postData.append('&');
+//            postData.append(URLEncoder.encode(param.getKey(), "UTF-8"));
+//            postData.append('=');
+//            postData.append(URLEncoder.encode(String.valueOf(param.getValue()), "UTF-8"));
+//        }
+//        byte[] postDataBytes = postData.toString().getBytes("UTF-8");
+//
+//        HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+//        conn.setRequestMethod("POST");
+//        conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+//        conn.setRequestProperty("Content-Length", String.valueOf(postDataBytes.length));
+//        conn.setDoOutput(true);
+//        conn.getOutputStream().write(postDataBytes);
+//
+//        BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+//
+//        StringBuilder everything = new StringBuilder();
+//        String line;
+//        while( (line = in.readLine()) != null) {
+//            everything.append(line);
+//        }
+//
+//        String reply = everything.toString();
+//
+//        Log.i("sense49", reply);
 
         setResult(Activity.RESULT_OK, null);
         finish();
